2eb0d606bae42f590e64f09ed2f2be3dcbe1dfdb
Giwan Kim <giwan.kim@gmail.com>
2025-04-19 16:47:01 +0900

    feat: Add validation error handling and enhance logging configuration
    Added a handler for `MethodArgumentNotValidException` to provide detailed validation error feedback to the client. Enhanced logging by introducing trace-level logging to improve issue diagnosis.

--- src/main/java/kr/co/shortenurlservice/presentation/GlobalExceptionHandler.java
+++ src/main/java/kr/co/shortenurlservice/presentation/GlobalExceptionHandler.java
@@ -5,6 +5,7 @@ import kr.co.shortenurlservice.domain.NotFoundShortenUrlException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -15,6 +16,7 @@ public class GlobalExceptionHandler {
   public ResponseEntity<String> handleLackOfShortenUrlKeyException(
       LackOfShortenUrlKeyException ex) {
     // 개발자에게 알려줄 수 있는 수단 필요
+    log.error("단축 URL 자원이 부족합니다.");
     return new ResponseEntity<>("단축 URL 자원이 부족합니다.", HttpStatus.INTERNAL_SERVER_ERROR);
   }
 
@@ -22,5 +24,25 @@ public class GlobalExceptionHandler {
   public ResponseEntity<String> handleNotFoundShortenUrlException(NotFoundShortenUrlException ex) {
     log.info(ex.getMessage());
     return new ResponseEntity<>("단축 URL을 찾지 못했습니다.", HttpStatus.NOT_FOUND);
+  }
+
+  @ExceptionHandler(MethodArgumentNotValidException.class)
+  public ResponseEntity<String> handleMethodArgumentNotValidException(
+      MethodArgumentNotValidException ex) {
+    // 유효성 검증 오류 세부 정보 추출
+    StringBuilder errorMessage = new StringBuilder().append("유효성 검증 실패: ");
+    ex.getBindingResult()
+        .getFieldErrors()
+        .forEach(
+            error -> {
+              errorMessage.append(
+                  String.format("필드 '%s': %s. ", error.getField(), error.getDefaultMessage()));
+            });
+
+    // 상세 로그
+    log.debug("잘못된 요청: {}", errorMessage);
+
+    // 클리아언트에 응답
+    return new ResponseEntity<>(errorMessage.toString(), HttpStatus.BAD_REQUEST);
   }
 }
